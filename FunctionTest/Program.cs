a048f57f72f80da1aeba3d5437106285ef096983
binghong he <1250407685@qq.com>
2016-09-28 22:39:22 +0800

    初始化提交

--- FunctionTest/Program.cs
+++ FunctionTest/Program.cs
@@ -10,7 +10,7 @@ namespace FunctionTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("HelloWorl!");//1235415651
+            Console.WriteLine("HelloWorl!");
         }
     }
 }
