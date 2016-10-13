1e28cb54c20d58091995fb1112dc8d329d9499cb
hebinghong <1250407685@qq.com>
2016-10-13 18:11:49 +0800

    解析Json字符串

--- FunctionTest/Program.cs
+++ FunctionTest/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace FunctionTest
 {
@@ -10,8 +7,30 @@ namespace FunctionTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("HelloWorl!");
-            StringToTxt.StringToTxtFile("sfsdf");
+            PinYinToHanZi();
+            Console.ReadLine();
+        }
+        /// <summary>
+        /// 字符输出到txt文件
+        /// </summary>
+        void StrToTxt()
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < 32; i++)
+            {
+                sb.AppendFormat("string var{0} = var{1}Index >= 0 ? dr[mydic[var{2}Index]].ToString() : \"\";", i, i, i);
+                sb.AppendLine();
+                Console.WriteLine(sb.ToString());
+            }
+            StringToTxt.StringToTxtFile(sb.ToString().Trim());
+        }
+        /// <summary>
+        /// 汉字转拼音
+        /// </summary>
+        public  static  void PinYinToHanZi()
+        {
+            Console.WriteLine(PinYinConverter.Get('峒'));
+            Console.WriteLine(PinYinConverter.Get("峒邕"));
         }
     }
 }
